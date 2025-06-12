aa4c1178526ef1d2a56345cf8075f047ac3c1266
CapstoneMaking <capstonemaking@gmail.com>
2025-06-12 10:57:10 +0800

    Add files via upload

---
+++ capstone/js files/register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD57lN0Vm_HIcotS9ZLb42th-mvX-pp7CI",
+    authDomain: "login-example-4748a.firebaseapp.com",
+    projectId: "login-example-4748a",
+    storageBucket: "login-example-4748a.firebasestorage.app",
+    messagingSenderId: "988590388972",
+    appId: "1:988590388972:web:284f385576a57e8b6917c9"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
